1c2646d2666062a4a7c8301b61119ff932e530d4
Thomas <tmyrup@gmail.com>
2025-04-30 15:10:55 +0200

    New version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1227490299"
+    serviceWorkerVersion: "2750015604"
   }
 });
